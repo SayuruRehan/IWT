3a6da7c5fa0ccc16d9ccecd85ab672a3923e2393
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-05-10 06:41:48 +0000

    lec7 textarea

---
+++ Lecture7-P1/textarea.php
@@ -0,0 +1,10 @@
+<?php
+is(isset($_POST['BtnSubmit']))
+  {
+  echo "<h2>Below is the form data</h2>";
+  echo "<hr>";
+  echo "</br>Your Name: {$_POST['Fullname']}";
+  echo "</br>Your Address: {$_POST['UserAddress']}";
+  echo "<hr>";
+  }
+?>
