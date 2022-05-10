ee0928319f32e8b150bcfa2373c2a1f98764d7fc
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-05-10 08:16:52 +0000

    lec7 p2

---
+++ Lecture7-P2/createfile.php
@@ -0,0 +1,6 @@
+<?php
+  $file = fopen("test.txt","r");
+  $file = fopen("/home/test/test.txt","r");
+  $file = fopen("http://www.example.com/","r");
+  $file = fopen("ftp://user:password@example.com/test.txt","w");
+?>
