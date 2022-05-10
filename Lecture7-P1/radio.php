94b6ecbde1fde045d1f585d5b11d0d88514e78d3
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-05-10 06:43:55 +0000

    lec7 radio

---
+++ Lecture7-P1/radio.php
@@ -0,0 +1,9 @@
+<?php
+  if(isset($_POST['BtnSubmit']))
+  {
+    echo "<h3>Your form data as bellow</h3>";
+    echo "</br>Your Name :{$_POST['FullName']}";
+    echo "</br>Your are :{$_POST['YourGender']}";
+    echo "<hr>";
+  }
+?>
