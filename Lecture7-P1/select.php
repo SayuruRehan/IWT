63942c8f843f478da3ed33b454a42d7203a48f4a
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-05-10 06:48:46 +0000

    lec7 select

---
+++ Lecture7-P1/select.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_POST['BtnSubmit']))
+{
+  echo "<h3>Your submitted form data as bellow</h3>";
+  echo "Your Name : {$_POST['FullName']}</br>";
+  echo "I like : <h2>{$_POST['I_like']} </h2> Programming...!</br>";
+  echo "<hr>";
+}
+?>
